8589340b8bc45a1d9cc3246fe4fc1d32edf330e4
Ryan Wang <thepoy@163.com>
2025-09-03 09:08:03 +0800

    feat(context): 添加 TooltipProvider 到应用上下文
    在 AppContextProvider 中新增 TooltipProvider 以支持工具提示功能

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,6 +1,6 @@
 import type { ParentProps } from "solid-js";
 
-import { ToastProvider } from "fluent-solid";
+import { ToastProvider, TooltipProvider } from "fluent-solid";
 
 import { ConfigContextProvider } from "./ConfigContext";
 import { SettingsContextProvider } from "./SettingsContext";
@@ -11,15 +11,19 @@ import { ParsingContextProvider } from "./ParsingContext";
 export const AppContextProvider = (props: ParentProps) => {
   return (
     <ToastProvider>
-      <ConfigContextProvider>
-        <SettingsContextProvider>
-          <HistoryContextProvider>
-            <ParsedResultContextProvider>
-              <ParsingContextProvider>{props.children}</ParsingContextProvider>
-            </ParsedResultContextProvider>
-          </HistoryContextProvider>
-        </SettingsContextProvider>
-      </ConfigContextProvider>
+      <TooltipProvider>
+        <ConfigContextProvider>
+          <SettingsContextProvider>
+            <HistoryContextProvider>
+              <ParsedResultContextProvider>
+                <ParsingContextProvider>
+                  {props.children}
+                </ParsingContextProvider>
+              </ParsedResultContextProvider>
+            </HistoryContextProvider>
+          </SettingsContextProvider>
+        </ConfigContextProvider>
+      </TooltipProvider>
     </ToastProvider>
   );
 };
